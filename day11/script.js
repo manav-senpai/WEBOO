51434e93c6747ee8b7a6a52ab293dcba9d6fc6ce
MANDANE MANN <rockstarsigma@gmail.com>
2025-07-04 03:12:01 +0530

    Add files via upload
    its the day 11 of js

---
+++ day11/script.js
@@ -0,0 +1,41 @@
+// let cars = ["honda", "mahindra", "bmw", "Toyota", "Tesla"];
+// let car = cars[0]
+
+// console.log(car)
+// console.log(cars.length)
+// for (let idx = 0; idx <= cars.length; idx++){
+
+//     console.log(cars[idx])
+// }
+
+//for of
+// for ( let car of cars){
+//     console.log(car);
+// }
+
+// assignment >> for a given array with marks of students -> [ 85, 97, 44, 37, 76, 60]. find the average marks of the entire class
+
+// let Marks = [ 85, 97, 44, 37, 76, 60];
+// let sum = 0;
+// console.log()
+// for (let mark of Marks){
+//     sum = sum + mark;
+// }
+// console.log("average of marks is :",sum / Marks.length)
+
+//question solved
+
+//assignment 2 >> for a given array with prices of 5 itmes>> 
+//all items have an offer of 10 % off on them. change the arry to store final price after applying offer..
+
+// let Prices = [ 250, 645, 300, 900, 50];
+// let PricesRev = [];
+
+// for(let i = 0; i <= Prices.length; i++){
+//     let discount = (Prices[i]/100)* 90;
+//     PricesRev.push(discount);
+// }
+// console.log("Before discount :", Prices);
+// console.log("After discount :", PricesRev);
+
+//question solved
