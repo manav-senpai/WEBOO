32011845e78e74687ac11852286fac6443d37449
MANDANE MANN <rockstarsigma@gmail.com>
2025-07-14 23:43:39 +0530

    Add files via upload

---
+++ day13/script.js
@@ -0,0 +1,10 @@
+// console.dir(document.body);
+// let head = document.getElementById("header");
+// console.log(head);
+
+// let headings = document.getElementsByClassName("heading");
+// console.dir(headings);
+// console.log(headings);
+
+let paras = document.getElementsByTagName("p");
+console.dir(paras);
